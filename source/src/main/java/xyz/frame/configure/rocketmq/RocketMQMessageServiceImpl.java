9edd401eaa2640f15e4db1e26f21bf43e34a72be
shisp <shisp@2345.com>
2017-11-27 23:35:18 +0800

    添加druid连接池

--- source/src/main/java/xyz/frame/configure/rocketmq/RocketMQMessageServiceImpl.java
+++ source/src/main/java/xyz/frame/configure/rocketmq/RocketMQMessageServiceImpl.java
@@ -11,8 +11,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import xyz.frame.pojo.common.ConfigKey;
-import xyz.frame.utils.ConfigurePropertiesIoStream;
 import xyz.frame.utils.ServiceException;
 
 /**
