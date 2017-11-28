6cbdc9d20dcd1780a19fed80e129dffc5fef3c4f
shisp <shisp@2345.com>
2017-11-28 19:59:05 +0800

    1.banner图 2.添加batchconfig提供bean 3.任务开关 4.mapper扫描 5.spring工具类获取bean等

--- source/src/main/java/xyz/frame/ServerApplication.java
+++ source/src/main/java/xyz/frame/ServerApplication.java
@@ -1,12 +1,16 @@
 package xyz.frame;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 /**
  * @Description 启动类
  * @author shisp
  * @date 2017年11月27日  下午2:28:20
  */
+@EnableWebMvc
+@MapperScan(basePackages = "xyz.frame.mapper")
 @SpringBootApplication
 public class ServerApplication {
 
---
+++ source/src/main/java/xyz/frame/configure/schedule/BatchConfigure.java
@@ -0,0 +1,22 @@
+package xyz.frame.configure.schedule;
+
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.scheduling.quartz.SchedulerFactoryBean;
+
+@Configuration
+@ComponentScan("xyz.frame")
+public class BatchConfigure {	
+	
+	@Bean(name = "schedulerFactoryBean")
+	@Scope(value = ConfigurableBeanFactory.SCOPE_SINGLETON)	
+	public SchedulerFactoryBean getSchedulerFactoryBean(BatchJobFactory batchJobFactory){
+		SchedulerFactoryBean schedulerFactoryBean = new SchedulerFactoryBean();
+		schedulerFactoryBean.setJobFactory(batchJobFactory);
+		return schedulerFactoryBean;
+	}
+	
+}
