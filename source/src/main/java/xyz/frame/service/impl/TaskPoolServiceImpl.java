7de334c82e7a6c66e0bf4e1af0e0f3692e43a943
soogbo <soogbo@163.com>
2017-12-09 16:33:03 +0800

    报错修改

--- source/src/main/java/xyz/frame/service/impl/TaskPoolServiceImpl.java
+++ source/src/main/java/xyz/frame/service/impl/TaskPoolServiceImpl.java
@@ -2,7 +2,6 @@ package xyz.frame.service.impl;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.Callable;
 import java.util.concurrent.Future;
 import java.util.concurrent.FutureTask;
 
@@ -16,52 +15,46 @@ import org.springframework.stereotype.Service;
 import xyz.frame.service.TaskPoolService;
 
 /**
- * @Description 
+ * @Description
  * @author shisp
  * @param <V>
- * @date 2017年11月28日  下午8:12:41
+ * @date 2017年11月28日 下午8:12:41
  */
 @Service("taskPoolService")
 public class TaskPoolServiceImpl<V> implements TaskPoolService {
 
-    private static final Logger logger = LoggerFactory.getLogger(TaskPoolServiceImpl.class); 
-    
-    @Resource(name="commonTaskExecutor")
-    private TaskExecutor taskExecutor;
-    private FutureTask taskExecutor2;
+	private static final Logger logger = LoggerFactory.getLogger(TaskPoolServiceImpl.class);
 
-    @Override
-    public List<Future<?>> testTaskPool() {
-        List<Future<?>> futureList = new ArrayList<>();
-        Future<?> future = null;
-        for (int i = 0; i < 10; i++) {
-            logger.info("for i={}", i);
-            final int t = i;
-            
-             taskExecutor.execute(new FutureTask<>(()->{
-                    return null;
-            }));
-            
-            /*future = taskExecutor.execute(() -> {
-                logger.info("for i={}", t);
-                boolean flag = true;
-                return flag;
-            });
-            futureList.add(future);*/
-            
-            
-           taskExecutor.execute(
-                    new Runnable() {
-                        @Override
-                        public void run() {
-                            
-                        }
-                    });
-        
-           
-        
-        return futureList;
-    }
-    
+	@Resource(name = "commonTaskExecutor")
+	private TaskExecutor taskExecutor;
+//	private FutureTask taskExecutor2;
 
+	@Override
+	public List<Future<?>> testTaskPool() {
+		List<Future<?>> futureList = new ArrayList<>();
+//		Future<?> future = null;
+		for (int i = 0; i < 10; i++) {
+			logger.info("for i={}", i);
+//			final int t = i;
+
+			taskExecutor.execute(new FutureTask<>(() -> {
+				return null;
+			}));
+
+			/*
+			 * future = taskExecutor.execute(() -> { logger.info("for i={}", t);
+			 * boolean flag = true; return flag; }); futureList.add(future);
+			 */
+
+			taskExecutor.execute(new Runnable() {
+				@Override
+				public void run() {
+
+				}
+			});
+
+		}
+		return futureList;
+
+	}
 }
